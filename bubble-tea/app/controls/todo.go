796805786f9ef573fba41979ad9d35065b0b1a00
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2022-04-01 06:23:09 +0400

    initial rendering

--- bubble-tea/app/controls/todo.go
+++ bubble-tea/app/controls/todo.go
@@ -2,6 +2,8 @@ package controls
 
 import (
 	"bubbletea/domain/models"
+	"bubbletea/domain/services"
+	"fmt"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -10,18 +12,38 @@ type TodoControl struct {
 	data     []models.Todo
 	cursor   int
 	selected map[int]struct{}
+	service  services.TodoService
 }
 
 func (ctrl *TodoControl) Init() tea.Cmd {
+	ctrl.data, _ = ctrl.service.ListTodos()
 	return nil
 }
 
 func (ctrl *TodoControl) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch(msg) := msg.(type) {
-		case tea.KeyMsg:
-			switch msg.String(){
-			case "q", "ctrl+c":
-				return ctrl, tea.Quit
-			}
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return ctrl, tea.Quit
+		}
 	}
+	return ctrl, nil
+}
+
+func (ctrl *TodoControl) View() string {
+	s := "Pending TODOs\n"
+	for i, todo := range ctrl.data {
+		cursor := " "
+		if ctrl.cursor == i {
+			cursor = ">"
+		}
+		s += fmt.Sprintf("%s %s\n", cursor, todo.Title)
+	}
+	s += "\npress q to quit\n"
+	return s
+}
+
+func NewTodoControl() *TodoControl {
+	return &TodoControl{}
 }
