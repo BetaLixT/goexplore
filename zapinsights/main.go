05a03e734da4b34c92bcf1800edcdcbd38ef0d57
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2022-04-07 05:50:36 +0400

    included waits for insights to sync

--- zapinsights/main.go
+++ zapinsights/main.go
@@ -48,7 +48,6 @@ func main() {
 
 	// client.TrackTrace("Log from test logger", contracts.Information)
 	// client.Channel().Flush()
-	fmt.Scanln()
 
 }
 
@@ -65,8 +64,26 @@ func NewInsightsSync(clnt appinsights.TelemetryClient) *InsightsSink {
 }
 
 func (ins *InsightsSink) Sync() error {
-	fmt.Println("Syncing")
-	ins.clnt.Channel().Flush()
+	fmt.Println("Waiting for insights to sync...")
+	ins.clnt.Channel().Close()
+	select {
+	case <-ins.clnt.Channel().Close(10 * time.Second):
+		// Ten second timeout for retries.
+
+		// If we got here, then all telemetry was submitted
+		// successfully, and we can proceed to exiting.
+	case <-time.After(30 * time.Second):
+		// Thirty second absolute timeout.  This covers any
+		// previous telemetry submission that may not have
+		// completed before Close was called.
+
+		// There are a number of reasons we could have
+		// reached here.  We gave it a go, but telemetry
+		// submission failed somewhere.  Perhaps old events
+		// were still retrying, or perhaps we're throttled.
+		// Either way, we don't want to wait around for it
+		// to complete, so let's just exit.
+	}
 	return nil
 }
 
