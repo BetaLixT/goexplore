9b942e5e59d7e7be0eedfc08970a4ce0caba8650
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2022-04-07 05:38:04 +0400

    quick test with zap and insights

--- zapinsights/main.go
+++ zapinsights/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/microsoft/ApplicationInsights-Go/appinsights"
 	"github.com/microsoft/ApplicationInsights-Go/appinsights/contracts"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
@@ -15,7 +16,8 @@ func main() {
 	fmt.Println("vim-go")
 	godotenv.Load()
 	key := os.Getenv("InstrumentationKey")
-	fmt.Printf("Utilizing key: %s\n", key)
+
+	// - insights
 	client := appinsights.NewTelemetryClient(key)
 
 	appinsights.NewDiagnosticsMessageListener(func(msg string) error {
@@ -23,8 +25,29 @@ func main() {
 		return nil
 	})
 
-	client.TrackTrace("Log from test logger", contracts.Information)
-	client.Channel().Flush()
+	// - zap
+	lvl := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return true
+	})
+	ins := zapcore.WriteSyncer(NewInsightsSync(client))
+	enc := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(enc, ins, lvl),
+	)
+
+	logger := zap.New(core)
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
+	sugar.Infow("Register Insights",
+		// Structured context as loosely typed key-value pairs.
+		"insightsKey", key,
+	)
+
+	// fmt.Printf("Utilizing key: %s\n", key)
+
+	// client.TrackTrace("Log from test logger", contracts.Information)
+	// client.Channel().Flush()
 	fmt.Scanln()
 
 }
@@ -35,12 +58,19 @@ type InsightsSink struct {
 
 var _ zapcore.WriteSyncer = (*InsightsSink)(nil)
 
+func NewInsightsSync(clnt appinsights.TelemetryClient) *InsightsSink {
+	return &InsightsSink{
+		clnt: clnt,
+	}
+}
+
 func (ins *InsightsSink) Sync() error {
+	fmt.Println("Syncing")
 	ins.clnt.Channel().Flush()
 	return nil
 }
 
 func (ins *InsightsSink) Write(data []byte) (int, error) {
-	ins.clnt.Channel().Flush()
-	return 0, nil
+	ins.clnt.TrackTrace(string(data), contracts.Information)
+	return len(data), nil
 }
