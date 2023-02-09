c380f8a6d34da474baf83f6af506ae118fc12ae0
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2023-02-09 18:35:26 +0400

    testing pipe

--- redis/main.go
+++ redis/main.go
@@ -1,22 +1,48 @@
-package redis
+package main
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
 
 func main() {
-  rdb := redis.NewClient()
-  rdb.Set()
-  rdb.AddHook()
+	rdb := redis.NewClient(
+		&redis.Options{
+			Addr: "127.0.0.1:6379",
+			DB:   0,
+		},
+	)
+	ctx := context.Background()
+	rdb.Set(ctx, "wow:a", "12", time.Hour)
+	rdb.Set(ctx, "wow:b", "12", time.Hour)
+	rdb.Set(ctx, "wow:c", "12", 0)
+	rdb.Set(ctx, "wow:d", "12", time.Hour)
+	rdb.Set(ctx, "wow:e", "12", time.Hour)
+	rdb.Set(ctx, "wow:f", "12", time.Hour)
+
+	pipe := rdb.Pipeline()
+	pipe.TTL(ctx, "wow:a")
+	pipe.TTL(ctx, "wow:b")
+	pipe.TTL(ctx, "wow:c")
+	res, err := pipe.Exec(ctx)
+	if err != nil {
+		println("failure lmao")
+	}
+	for idx := range res {
+		dur, ok :=  (res[idx]).(*redis.DurationCmd)
+		if !ok {
+			println("wtf why?")
+		} else {
+			print((dur.Args()[1]).(string))
+			println(dur.Val())
+		}
+	}
+	println("lmao")
 }
 
-
 type Client struct {
-
 }
 
 type cmdable func(ctx context.Context, str string) error
-
-
